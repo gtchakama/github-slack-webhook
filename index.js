b9e505c3c60547df22d2c9a4b1cb6e416a65735f
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2023-06-02 12:41:40 +0200

    update

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { WebClient } = require('@slack/web-api');
+// Import dependencies
+const { Octokit } = require("@octokit/rest");
+const dotenv = require('dotenv');
+const Slack = require('slack-node');
 
-// Create a new Express app
-const app = express();
+// Load environment variables from .env file
+dotenv.config();
 
-// Use body-parser middleware to parse incoming requests
-app.use(bodyParser.json());
+// Initialize Octokit REST client
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
 
-// Initialize the Slack Web API client
-const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
+// Initialize Slack client
+const slack = new Slack();
+slack.setWebhook(process.env.SLACK_WEBHOOK_URL);
 
-// Define the Slack channel to send messages to
-const slackChannel = 'github-george';
+// Listen for pull request events on GitHub repo
+octokit.activity.listRepoEvents({
+    owner: 'gtchakama',
+    repo: 'gt-chakama',
+    per_page: 100 // Set the number of events to return
+}).then(result => {
+    const pullRequests = result.data.filter(event => event.type === "PullRequestEvent" && event.payload.action === "opened")
+    // Send messages to Slack for each new pull request
+    pullRequests.forEach(pullRequest => {
+        const message = `New Pull Request: ${pullRequest.payload.pull_request.title} - ${pullRequest.payload.pull_request.html_url}`;
+        sendSlackMessage(message);
+    });
+}).catch(err => {
+    console.error(`Error getting pull requests: ${err}`);
+});
 
-// Define the route that will receive the GitHub webhook payload
-app.post('/github-webhook', async (req, res) => {
-  const payload = req.body;
-
-  // Check if the payload is a pull request event and if it's a new pull request
-  if (payload.action === 'opened' && payload.pull_request) {
-    try {
-      // Get the relevant information from the payload
-      const pullRequestId = payload.number;
-      const pullRequestTitle = payload.pull_request.title;
-      const pullRequestUrl = payload.pull_request.html_url;
-
-      // Construct the message to send to Slack
-      const message = `New Pull Request created!\nID: ${pullRequestId}\nTitle: ${pullRequestTitle}\nURL: ${pullRequestUrl}`;
-
-      // Send the message to Slack
-      await slackClient.chat.postMessage({
-        channel: slackChannel,
-        text: message,
-      });
-      console.log('Message sent to Slack');
-    } catch (error) {
-      console.error(error);
+// Function to send messages to Slack
+function sendSlackMessage(message) {
+  slack.webhook({
+    text: message,
+    channel: "github-george",
+    username: "GitHub Pull Requests"
+  }, function(err, response) {
+    if (err) {
+      console.error(`Error sending message: ${err}`);
+    } else {
+      console.log(`Message sent: ${message}`);
     }
-  }
-
-  res.sendStatus(200);
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+  });
+}
